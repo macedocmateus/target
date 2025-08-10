bef3297bc68dba62164063b5b2d6431b4284b4cc
mateus <mateusmacedoeng@gmail.com>
2025-08-10 10:48:25 -0300

    add formatter and lint using biomejs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
     return (
-        <View style={{flex: 1, justifyContent: "center"}}>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
             <Text>Olá, Expo Router</Text>
         </View>
-    )
+    );
 }
