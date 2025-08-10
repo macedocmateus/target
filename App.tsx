f199320974f0541394fb94a2acb542069abe0f55
mateus <mateusmacedoeng@gmail.com>
2025-08-09 22:07:30 -0300

    add theme and assets

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
 
-import {  Text, View } from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
+
+import {colors} from '@/theme/colors'
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>
         Oi, React Native
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
