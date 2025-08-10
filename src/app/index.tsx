b3c93a45b1bfe8a9851bfbe557999a0ab68a4b14
mateus <mateusmacedoeng@gmail.com>
2025-08-10 11:07:35 -0300

    add and config  expo router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { Button, Text, View } from 'react-native';
 
 export default function Index() {
     return (
         <View style={{ flex: 1, justifyContent: 'center' }}>
             <Text>Olá, Expo Router</Text>
+            <Button
+                title="Ir para Target"
+                onPress={() => router.navigate('/target')}
+            />
         </View>
     );
 }
