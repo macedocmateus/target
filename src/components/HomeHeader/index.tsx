1524d911756397ba94f536da0d8e79845ca27639
mateus <mateusmacedoeng@gmail.com>
2025-08-14 22:11:30 -0300

    reusing types

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,12 +1,14 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { Text, View } from 'react-native';
 import { Separator } from '@/components/Separator';
-import { Summary } from '@/components/Summary';
+import { Summary, type SummaryProps } from '@/components/Summary';
 import { colors } from '@/theme/colors';
 import { styles } from './styles';
 
 export type HomeHeaderProps = {
     total: string;
+    input: SummaryProps;
+    output: SummaryProps;
 };
 
 type Props = {
@@ -28,12 +30,12 @@ export function HomeHeader({ data }: Props) {
 
             <View style={styles.summary}>
                 <Summary
-                    data={{ label: 'Entradas', value: 'R$ 6,184.90' }}
+                    data={data.input}
                     icon={{ name: 'arrow-upward', color: colors.green[500] }}
                 />
 
                 <Summary
-                    data={{ label: 'Saídas', value: '-R$ 883.65' }}
+                    data={data.output}
                     icon={{ name: 'arrow-downward', color: colors.red[400] }}
                     isRight
                 />
