d2600ba23be8c96634197b8aada3d23b57408459
mateus <mateusmacedoeng@gmail.com>
2025-08-22 07:44:55 -0300

    add transaction type component

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,12 +1,16 @@
 import { useLocalSearchParams } from 'expo-router';
+import { useState } from 'react';
 import { View } from 'react-native';
 import { Button } from '@/components/Button';
 import { CurrencyInput } from '@/components/CurrencyInput';
 import { Input } from '@/components/Input';
 import { PageHeader } from '@/components/PageHeader';
+import { TransactionType } from '@/components/TransactionType';
+import { TransactionTypes } from '@/utils/TransactionTypes';
 
 export default function Transaction() {
     const params = useLocalSearchParams<{ id: string }>();
+    const [type, setType] = useState(TransactionTypes.Input);
 
     return (
         <View style={{ flex: 1, padding: 24 }}>
@@ -16,6 +20,7 @@ export default function Transaction() {
             />
 
             <View style={{ marginTop: 32, gap: 24 }}>
+                <TransactionType selected={type} onChange={setType} />
                 <CurrencyInput label="Valor (R$)" value={0} />
                 <Input
                     label="Motivo (opcional)"
