70fc521d532a98b8d0d4f40184634bc4b732bca6
mateus <mateusmacedoeng@gmail.com>
2025-08-25 17:23:13 -0300

    add remove method

--- src/database/useTargetDataBase.ts
+++ src/database/useTargetDataBase.ts
@@ -81,10 +81,15 @@ export function useTargetDatabase() {
         });
     }
 
+    async function remove(id: number) {
+        await database.runAsync('DELETE FROM targets WHERE id = ?', id);
+    }
+
     return {
         create,
         listBySavedValue,
         show,
         update,
+        remove,
     };
 }
