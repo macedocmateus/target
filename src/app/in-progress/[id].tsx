5642328a8c72766689838d61baa7be108c26c039
mateus <mateusmacedoeng@gmail.com>
2025-08-25 16:05:04 -0300

    add show goal method and format currency

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,17 +1,15 @@
-import { router, useLocalSearchParams } from 'expo-router';
-import { View } from 'react-native';
+import { router, useFocusEffect, useLocalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { Alert, View } from 'react-native';
 import { Button } from '@/components/Button';
 import { List } from '@/components/List';
+import { Loading } from '@/components/Loading';
 import { PageHeader } from '@/components/PageHeader';
 import { Progress } from '@/components/Progress';
 import { Transaction, type TransactionProps } from '@/components/Transaction';
+import { useTargetDatabase } from '@/database/useTargetDataBase';
+import { numberToCurrency } from '@/utils/numberToCurrency';
 import { TransactionTypes } from '@/utils/TransactionTypes';
-
-const details = {
-    current: 'R$ 580.00',
-    target: 'R$ 1790.00',
-    percentage: 25,
-};
 
 const transactions: TransactionProps[] = [
     {
@@ -32,11 +30,55 @@ const transactions: TransactionProps[] = [
 
 export default function InProgress() {
     const params = useLocalSearchParams<{ id: string }>();
+    const targetDatabase = useTargetDatabase();
+
+    const [isFetching, setIsFetching] = useState(true);
+    const [details, setDetails] = useState({
+        name: 'ola',
+        current: 'R$ 0,00',
+        target: 'R$ 0,00',
+        percentage: 0,
+    });
+
+    async function fetchDetails() {
+        try {
+            const response = await targetDatabase.show(Number(params.id));
+            setDetails({
+                name: response?.name,
+                current: numberToCurrency(response?.current),
+                target: numberToCurrency(response?.amount),
+                percentage: response?.percentage,
+            });
+        } catch (error) {
+            Alert.alert(
+                'Erro',
+                'Não foi possível carregar os detalhes da meta.',
+            );
+            console.log(error);
+        }
+    }
+
+    async function fetchData() {
+        const fetchDetailsPromise = fetchDetails();
+
+        await Promise.all([fetchDetailsPromise]);
+        setIsFetching(false);
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchData();
+        }, []),
+    );
+
+    if (isFetching) {
+        return <Loading />;
+    }
 
     return (
         <View style={{ flex: 1, padding: 24, gap: 32 }}>
             <PageHeader
-                title="Apple Watch"
+                title={details.name}
                 rightButton={{
                     icon: 'edit',
                     onPress: () => {},
--- src/database/useTargetDataBase.ts
+++ src/database/useTargetDataBase.ts
@@ -45,8 +45,25 @@ export function useTargetDatabase() {
             `);
     }
 
+    function show(id: number) {
+        return database.getFirstAsync<TargetResponse>(`
+            SELECT
+             targets.id,
+             targets.name,
+             targets.amount,
+             COALESCE(SUM(transactions.amount), 0) AS current,
+             COALESCE((SUM(transactions.amount) / targets.amount) * 100, 0) AS percentage,
+             targets.created_at,
+             targets.updated_at
+            FROM targets
+            LEFT JOIN transactions ON targets.id = transactions.target_id
+            WHERE targets.id = ${id}
+            `);
+    }
+
     return {
         create,
         listBySavedValue,
+        show,
     };
 }
